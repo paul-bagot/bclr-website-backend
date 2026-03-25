1e4f81f19183ddbc9986216ef0912db6130d182a
bclr <120285568+paul-bagot@users.noreply.github.com>
2026-03-25 23:06:50 +0100

    Simplify PrismaService constructor
    Refactored PrismaService constructor to use default datasource configuration.

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -5,13 +5,7 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient {
 	constructor(config: ConfigService) {
-		super({
-			datasources: {
-				db: {
-					url: config.get('DATABASE_URL'),
-				},
-			},
-		});
+		super();
 		console.log(config.get('DATABASE_URL'));
 	}
 }
